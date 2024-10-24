8ae362d3fc494a69653c6dbd8a63c8811ed1655d
Axel Bellec <axel@synapse-medicine.com>
2024-10-25 00:30:42 +0200

    feat: implement file caching for racks data

--- src/routes/api/racks/+server.ts
+++ src/routes/api/racks/+server.ts
@@ -2,8 +2,7 @@ import fs from 'fs/promises';
 import path from 'path';
 import fetch from 'node-fetch';
 
-import racks from './racks.json'
-
+const racksFilePath = path.resolve('./src/routes/api/racks/racks.json');
 const racksUrl = 'https://opendata.bordeaux-metropole.fr/api/explore/v2.1/catalog/datasets/st_arceau_p/exports/geojson?lang=fr&timezone=Europe%2FBerlin';
 
 async function fetchAndCacheRacks() {
@@ -17,19 +16,19 @@ async function fetchAndCacheRacks() {
     throw new Error('Failed to fetch racks data');
   }
   const data = await response.json();
-  // await fs.writeFile(racksFilePath, JSON.stringify(data, null, 2));
+  await fs.writeFile(racksFilePath, JSON.stringify(data, null, 2));
   return data;
 }
 
 export async function GET(event) {
-  // try {
-  //   // Check if racks.json exists
-  //   await fs.access(racksFilePath);
-  //   // If it exists, read from the file
-  //   console.log('racks.json exists');
-  //   const racks = await fs.readFile(racksFilePath, 'utf-8');
-  //   return new Response(racks, { status: 200 });
-  // } catch (error) {
+  try {
+    // Check if racks.json exists
+    await fs.access(racksFilePath);
+    // If it exists, read from the file
+    console.log('racks.json exists');
+    const racks = await fs.readFile(racksFilePath, 'utf-8');
+    return new Response(racks, { status: 200 });
+  } catch (error) {
     // If it doesn't exist, fetch and cache it
     try {
       console.log('racks.json does not exist, fetching and caching it');
@@ -38,5 +37,5 @@ export async function GET(event) {
     } catch (fetchError) {
       return new Response('Failed to fetch racks data', { status: 500 });
     }
-  // }
+  }
 }
