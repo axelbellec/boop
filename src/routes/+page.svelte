1af5fb09446d632aa12dd9416e96258a14de3c54
Axel Bellec <axel@synapse-medicine.com>
2024-10-25 13:28:53 +0200

    feat: refactor map initialization and event handling in Svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,277 +1,267 @@
 <script lang="ts">
-    import mapboxgl from "mapbox-gl";
-    import { onMount } from "svelte";
-    import type { MapMouseEvent } from 'mapbox-gl';
+  import mapboxgl from "mapbox-gl";
+  import { onMount } from "svelte";
+  import type { MapMouseEvent } from "mapbox-gl";
 
-    let map: mapboxgl.Map;
+  let map: mapboxgl.Map;
 
-    onMount(() => {
-      mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+  onMount(() => {
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-      map = new mapboxgl.Map({
-        container: "map", // container ID
-        center: [-0.5792, 44.8378],
-        zoom: 13,
-      });
-
-      // Initialize map controls here
-      initializeMapControls();
-
-      // Wait for both map to load and data to be fetched
-      Promise.all([
-        new Promise((resolve) => map.on('load', resolve)),
-        fetch('/api/racks')
-          .then((response) => response.json())
-          .then((data: GeoJSONData) => {
-            // Use the modular function to map the properties
-            data.features = data.features.map(mapFeatureProperties);
-            return data;
-          })
-      ]).then(([_, data]) => {
-        handleLoadedData(data);
-      }).catch((error) => console.error("Error loading map or GeoJSON data:", error));
+    map = new mapboxgl.Map({
+      container: "map", // container ID
+      center: [-0.5792, 44.8378],
+      zoom: 13,
     });
 
-    function initializeMapControls() {
-      // Add geolocate control
-      const geolocate = new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-      });
-      map.addControl(geolocate);
+    // Initialize map controls here
+    initializeMapControls();
 
-      // Add navigation control
-      const nav = new mapboxgl.NavigationControl();
-      map.addControl(nav, "top-right");
+    // Wait for both map to load and data to be fetched
+    Promise.all([
+      new Promise((resolve) => map.on("load", resolve)),
+      fetch("/api/racks")
+        .then((response) => response.json())
+        .then((data: GeoJSONData) => {
+          // Use the modular function to map the properties
+          data.features = data.features.map(mapFeatureProperties);
+          return data;
+        }),
+    ])
+      .then(([_, data]) => {
+        handleLoadedData(data);
+      })
+      .catch((error) =>
+        console.error("Error loading map or GeoJSON data:", error)
+      );
+  });
 
-      // Adjust map size on window resize
-      window.addEventListener("resize", () => {
-        map.resize();
-      });
-    }
+  function initializeMapControls() {
+    // Add geolocate control
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+    });
+    map.addControl(geolocate);
 
-    function handleLoadedData(data: GeoJSONData) {
-      console.log("Data loaded");
-      addMapLayers(data);
-      // Add event listeners for map interactions
-      addMapEventListeners();
-    }
+    // Add navigation control
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, "top-right");
 
-    function addMapEventListeners() {
-      // Add click event for clusters
-      map.on("click", "clusters", handleClusterClick);
+    // Adjust map size on window resize
+    window.addEventListener("resize", () => {
+      map.resize();
+    });
+  }
 
-      // Change cursor to pointer when hovering over clusters
-      map.on("mouseenter", "clusters", () => {
-        map.getCanvas().style.cursor = "pointer";
-      });
+  function handleLoadedData(data: GeoJSONData) {
+    console.log("Data loaded");
+    addMapLayers(data);
+    // Add event listeners for map interactions
+    addMapEventListeners();
+  }
 
-      // Change cursor back when not hovering over clusters
-      map.on("mouseleave", "clusters", () => {
-        map.getCanvas().style.cursor = "";
-      });
+  function addMapEventListeners() {
+    // Add click event for clusters
+    map.on("click", "clusters", handleClusterClick);
 
-      // Create a popup, but don't add it to the map yet.
-      const popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-        maxWidth: "300px",
-      });
+    // Change cursor to pointer when hovering over clusters
+    map.on("mouseenter", "clusters", () => {
+      map.getCanvas().style.cursor = "pointer";
+    });
 
-      map.on("mouseenter", "unclustered-point", (e) => {
-        map.getCanvas().style.cursor = "pointer";
-        handleUnclusteredPointInteraction(e, popup);
-      });
+    // Change cursor back when not hovering over clusters
+    map.on("mouseleave", "clusters", () => {
+      map.getCanvas().style.cursor = "";
+    });
 
-      map.on("mouseleave", "unclustered-point", () => {
-        map.getCanvas().style.cursor = "";
-        popup.remove();
-      });
+    // Create a popup, but don't add it to the map yet.
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+      maxWidth: "300px",
+    });
 
-      // Add touch event for mobile devices
-      map.on("click", "unclustered-point", (e) => {
-        handleUnclusteredPointInteraction(e, popup);
-      });
-    }
+    map.on("mouseenter", "unclustered-point", (e) => {
+      map.getCanvas().style.cursor = "pointer";
+      handleUnclusteredPointInteraction(e, popup);
+    });
 
-    // Define types for your data structures
-    type FeatureProperties = {
-      typologie: string;
-      nombre: number;
-      [key: string]: any;
+    map.on("mouseleave", "unclustered-point", () => {
+      map.getCanvas().style.cursor = "";
+      popup.remove();
+    });
+
+    // Add touch event for mobile devices
+    map.on("click", "unclustered-point", (e) => {
+      handleUnclusteredPointInteraction(e, popup);
+    });
+  }
+
+  // Define types for your data structures
+  type FeatureProperties = {
+    typologie: string;
+    nombre: number;
+    [key: string]: any;
+  };
+
+  type Feature = {
+    properties: FeatureProperties;
+    [key: string]: any;
+  };
+
+  type GeoJSONData = {
+    features: Feature[];
+    [key: string]: any;
+  };
+
+  // Function to get typology text
+  function getTypologyText(typologie: string): string {
+    const typologyMap: { [key: string]: string } = {
+      ARCEAU_MOTO: "Motorbike",
+      ARCEAU_VELO: "Bike",
+      ARCEAU_VELO_CARGO: "Bike + Cargo",
     };
+    return typologyMap[typologie] || "Unknown";
+  }
 
-    type Feature = {
-      properties: FeatureProperties;
-      [key: string]: any;
+  // Function to map feature properties to new names
+  function mapFeatureProperties(feature: Feature): Feature {
+    const { typologie, nombre, ...rest } = feature.properties;
+    return {
+      ...feature,
+      properties: {
+        ...rest,
+        rackTypology: getTypologyText(typologie),
+        rackCount: nombre,
+      },
     };
+  }
 
-    type GeoJSONData = {
-      features: Feature[];
-      [key: string]: any;
-    };
-
-    // Function to get typology text
-    function getTypologyText(typologie: string): string {
-      const typologyMap: { [key: string]: string } = {
-        ARCEAU_MOTO: "Motorbike",
-        ARCEAU_VELO: "Bike",
-        ARCEAU_VELO_CARGO: "Bike + Cargo",
-      };
-      return typologyMap[typologie] || "Unknown";
-    }
-
-    // Function to map feature properties to new names
-    function mapFeatureProperties(feature: Feature): Feature {
-      const { typologie, nombre, ...rest } = feature.properties;
-      return {
-        ...feature,
-        properties: {
-          ...rest,
-          rackTypology: getTypologyText(typologie),
-          rackCount: nombre,
-        },
-      };
-    }
-
-    // Function to create a popup description
-    function createPopupDescription(
-      rackTypology: string,
-      rackCount: number
-    ): string {
-      return `
+  // Function to create a popup description
+  function createPopupDescription(
+    rackTypology: string,
+    rackCount: number
+  ): string {
+    return `
               <strong>Rack Type</strong><br> 
               ${rackTypology}<br><br>
               <strong>Racks Available</strong><br>
               ${rackCount || "N/A"}
           `;
+  }
+
+  // Function to handle cluster click
+  function handleClusterClick(e: MapMouseEvent & EventData) {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: ["clusters"],
+    }) as mapboxgl.GeoJSONFeature[];
+
+    if (features.length === 0) return;
+
+    const clusterId = features[0].properties?.cluster_id;
+    if (!clusterId) return;
+
+    map.getSource("racks")?.getClusterExpansionZoom(clusterId, (err, zoom) => {
+      if (err || !zoom) return;
+
+      map.easeTo({
+        center: features[0].geometry?.coordinates as mapboxgl.LngLatLike,
+        zoom: zoom,
+      });
+    });
+  }
+
+  // Function to handle unclustered point interaction
+  function handleUnclusteredPointInteraction(
+    e: MapMouseEvent & EventData,
+    popup: mapboxgl.Popup
+  ) {
+    const coordinates = e.features?.[0].geometry?.coordinates.slice() as [
+      number,
+      number,
+    ];
+    const { rackTypology, rackCount } = e.features?.[0].properties || {};
+
+    const description = createPopupDescription(rackTypology, rackCount);
+
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    // Function to handle cluster click
-    function handleClusterClick(e: MapMouseEvent & EventData) {
-      const features = map.queryRenderedFeatures(e.point, {
-        layers: ["clusters"],
-      }) as mapboxgl.GeoJSONFeature[];
+    popup.setLngLat(coordinates).setHTML(description).addTo(map);
+  }
 
-      if (features.length === 0) return;
+  // Function to add map layers
+  function addMapLayers(data: GeoJSONData) {
+    map.addSource("racks", {
+      type: "geojson",
+      data: data as unknown as GeoJSON,
+      cluster: true,
+      clusterMaxZoom: 16,
+      clusterRadius: 50,
+    });
 
-      const clusterId = features[0].properties?.cluster_id;
-      if (!clusterId) return;
+    map.addLayer({
+      id: "clusters",
+      type: "circle",
+      source: "racks",
+      filter: ["has", "point_count"],
+      paint: {
+        "circle-color": [
+          "step",
+          ["get", "point_count"],
+          "#993D51",
+          100,
+          "#732232",
+          750,
+          "#4A0E18",
+        ],
+        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+      },
+    });
 
-      map.getSource("racks")?.getClusterExpansionZoom(clusterId, (err, zoom) => {
-        if (err || !zoom) return;
+    map.addLayer({
+      id: "cluster-count",
+      type: "symbol",
+      source: "racks",
+      filter: ["has", "point_count"],
+      layout: {
+        "text-field": "{point_count_abbreviated}",
+        "text-font": ["Rubik Medium", "Arial Unicode MS Bold"],
+        "text-size": 12,
+      },
+      paint: {
+        "text-color": "#FAF7F5", // Set text color to white
+      },
+    });
 
-        map.easeTo({
-          center: features[0].geometry?.coordinates as mapboxgl.LngLatLike,
-          zoom: zoom,
-        });
-      });
-    }
+    map.addLayer({
+      id: "unclustered-point",
+      type: "circle",
+      source: "racks",
+      filter: ["!", ["has", "point_count"]],
+      paint: {
+        "circle-color": [
+          "match",
+          ["get", "rackTypology"],
+          "Motorbike",
+          "#FFC4DB",
+          "Bike",
+          "#993D51",
+          "Bike + Cargo",
+          "#BF6076",
+          /* other */ "#ccc",
+        ],
+        "circle-radius": 7,
+        "circle-stroke-width": 2,
+        "circle-stroke-color": "#fff",
+      },
+    });
+  }
+</script>
 
-    // Function to handle unclustered point interaction
-    function handleUnclusteredPointInteraction(
-      e: MapMouseEvent & EventData,
-      popup: mapboxgl.Popup
-    ) {
-      const coordinates = e.features?.[0].geometry?.coordinates.slice() as [
-        number,
-        number,
-      ];
-      const { rackTypology, rackCount } = e.features?.[0].properties || {};
-
-      const description = createPopupDescription(rackTypology, rackCount);
-
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
-
-      popup.setLngLat(coordinates).setHTML(description).addTo(map);
-    }
-
-    // Function to add map layers
-    function addMapLayers(data: GeoJSONData) {
-      map.addSource("racks", {
-        type: "geojson",
-        data: data as unknown as GeoJSON,
-        cluster: true,
-        clusterMaxZoom: 16,
-        clusterRadius: 50,
-      });
-
-      map.addLayer({
-        id: "clusters",
-        type: "circle",
-        source: "racks",
-        filter: ["has", "point_count"],
-        paint: {
-          "circle-color": [
-            "step",
-            ["get", "point_count"],
-            "#993D51",
-            100,
-            "#732232",
-            750,
-            "#4A0E18",
-          ],
-          "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
-        },
-      });
-
-      map.addLayer({
-        id: "cluster-count",
-        type: "symbol",
-        source: "racks",
-        filter: ["has", "point_count"],
-        layout: {
-          "text-field": "{point_count_abbreviated}",
-          "text-font": ["Rubik Medium", "Arial Unicode MS Bold"],
-          "text-size": 12,
-        },
-        paint: {
-          "text-color": "#FAF7F5", // Set text color to white
-        },
-      });
-
-      map.addLayer({
-        id: "unclustered-point",
-        type: "circle",
-        source: "racks",
-        filter: ["!", ["has", "point_count"]],
-        paint: {
-          "circle-color": [
-            "match",
-            ["get", "rackTypology"],
-            "Motorbike",
-            "#FFC4DB",
-            "Bike",
-            "#993D51",
-            "Bike + Cargo",
-            "#BF6076",
-            /* other */ "#ccc",
-          ],
-          "circle-radius": 7,
-          "circle-stroke-width": 2,
-          "circle-stroke-color": "#fff",
-        },
-      });
-    }
-  </script>
-
-  <h1>Bordeaux <span>🚲</span> Racks</h1>
-  <div id="map"></div>
-  <button id="infoButton">ℹ️</button>
-  <div id="aboutSection" class="hidden">
-    <h3>About</h3>
-    <div>
-      <p>I needed a way to secure my bike, but I couldn't find any bike racks.</p>
-      <p>So, I built a website that maps out bike rack locations in Bordeaux.</p>
-      <p>
-        It pulls data from the Bordeaux Metropole Open Data platform to show
-        various rack types, including ones for motorbikes, standard bikes, and
-        cargo bikes.
-      </p>
-      <p>Click on a rack to see how many hoops are available.</p>
-    </div>
-  </div>
+<h1>Bordeaux <span>🚲</span> Racks</h1>
+<div id="map"></div>
